dbfa3d700b3d7219795991b04d32ca533d9ed937
iffatazriel <iffatazrielfitrianto@gmail.com>
2025-06-20 14:16:11 +0700

    Update Firebase admin init to use environment variable

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,9 +1,20 @@
 // src/config/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
+
+// Pastikan nama environment variable sesuai dengan yang Anda set di Railway
+// Di Railway Anda set Key: GOOGLE_APPLICATION_CREDENTIALS_JSON
+const serviceAccountJson = process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON; 
+
+if (!serviceAccountJson) {
+  console.error("GOOGLE_APPLICATION_CREDENTIALS_JSON environment variable is not set.");
+  process.exit(1); 
+}
+
+// Parse string JSON menjadi objek JavaScript
+const serviceAccount = JSON.parse(serviceAccountJson);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
